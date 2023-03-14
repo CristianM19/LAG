b3572eda6e5b1fa7d02341ee6d54dd08cbd57575
CristianM19 <cristiankmc20014@gmail.com>
2023-03-13 19:36:25 -0500

    Menu de l proyecto

---
+++ lookatgame/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+    <nav>
+      <ul className="Menu">
+        <li>Look at game</li>
+        <li>Menu</li>
+        <li>Categorias</li>
+        <li>Productos</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+    </>
+  )
+}
+
+export default App
+
